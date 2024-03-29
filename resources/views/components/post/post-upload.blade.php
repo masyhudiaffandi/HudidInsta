46444c4431c7c874a403606b47b0db13ac26b8be
masyhudiaffandi <masyhudiaffandi816@gmail.com>
2024-03-29 20:45:47 +0700

    feat: :heavy_minus_sign: remove daisy ui

--- resources/views/components/post/post-upload.blade.php
+++ resources/views/components/post/post-upload.blade.php
@@ -1,12 +1,16 @@
-<div class="py-12">
-    <div class="max-w-xl mx-auto sm:px-6 lg:px-8">
+<div class="pb-2">
+    <div class="max-w-xl mx-auto sm:px-6 lg:px-8 glass">
         <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
             <div class="p-6 text-gray-900">
                 <form action="{{ route('post.store') }}" method="post" enctype="multipart/form-data">
                     @csrf
                     <input type="text" name="user_id" value="{{ Auth::user()->id }}" class="hidden">
                     <input type="file" name="image">
-                    <input type="text" name="caption">
+                    <div class="mt-4">
+                        <x-input-label for="email" :value="__('Email')" />
+                        <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
+                        <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                    </div>
                     <input type="submit" class="btn btn-primary">
                 </form>
             </div>
