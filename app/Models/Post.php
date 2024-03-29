aec08cd04523af3eda4cf0ca5ca4386b18ae7957
masyhudiaffandi <masyhudiaffandi816@gmail.com>
2024-03-29 08:27:01 +0700

    feat: :card_file_box: add table relation for Post, User, Like table

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,25 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'caption',
+        'image',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function likes()
+    {
+        return $this->hasMany(Like::class);
+    }
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
 }
