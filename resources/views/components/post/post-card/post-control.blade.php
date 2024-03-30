5d2064d9bb15f188bc9344429fbf2143b6552958
masyhudiaffandi <masyhudiaffandi816@gmail.com>
2024-03-30 07:06:55 +0700

    feat: :bug: error when user not logged in

--- resources/views/components/post/post-card/post-control.blade.php
+++ resources/views/components/post/post-card/post-control.blade.php
@@ -1,7 +1,6 @@
 <x-dropdown>
-    <x-slot name="trigger" >
+    <x-slot name="trigger">
         <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
-
             <div class="button">
                 <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
                     <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
@@ -11,17 +10,32 @@
     </x-slot>
 
     <x-slot name="content">
-        <div class="controller p-2">
-            <div class="edit-btn">
-                <a href="edit">Edit</a>
+        @if(isset(Auth::user()->id))
+            <div class="controller p-2">
+                @if(auth()->user()->id !== $post->user_id)
+                    <div class="share-btn">
+                        <a href="#">Share</a>
+                    </div>
+                @else
+                    <div class="share-btn">
+                        <a href="#">Share</a>
+                    </div>
+                    <div class="edit-btn">
+                        <a href="edit">Edit</a>
+                    </div>
+                    <div class="delete-btn">
+                        <form action="{{ route('post.destroy', $post->id) }}" method="post">
+                            @csrf
+                            @method('DELETE')
+                            <x-primary-button>Delete</x-primary-button>
+                        </form>
+                    </div>
+                @endif
             </div>
-            <div class="delete-btn">
-                <form action="{{ route('post.destroy', $post->id) }}" method="post">
-                    @csrf
-                    @method('DELETE')
-                    <x-primary-button>Delete</x-primary-button>
-                </form>
+        @else
+            <div class="share-btn">
+                <a href="#">Share</a>
             </div>
-        </div>
+        @endif
     </x-slot>
 </x-dropdown>
