69d9cdbd7b184aa2a4f7336f7fc6247cc87e00e4
masyhudiaffandi <masyhudiaffandi816@gmail.com>
2024-03-30 09:08:06 +0700

    feat: :triangular_flag_on_post: modal can closed without posting

--- resources/views/components/post/post-upload.blade.php
+++ resources/views/components/post/post-upload.blade.php
@@ -3,43 +3,46 @@
 </button>
  
 <div id="myModal" class="modal fixed w-full h-full top-0 left-0 flex items-center justify-center hidden z-50">
- <div class="modal-overlay absolute w-full h-full bg-gray-900 opacity-50"></div>
- <div class="modal-container bg-white w-11/12 md:max-w-md mx-auto rounded shadow-lg z-50 overflow-y-auto transform transition-all duration-300 scale-0">
-   @if(@isset(Auth::user()->id))
-     <div class="upload-form">
-         <div class="max-w-xl mx-auto sm:px-6 lg:px-8 glass">
-             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                 <div class="p-6 text-gray-900 ">
-                     <form action="{{ route('post.store') }}" method="post" enctype="multipart/form-data">
-                         @csrf
-                         <input 
-                             type="text" 
-                             name="user_id" 
-                             value="{{ Auth::user()->id }}" 
-                             class="hidden"
-                         >
-                         <input type="file" name="image">
-                         <div class="mt-4">
-                             <x-input-label for="Caption" :value="__('Caption')" />
-                             <x-text-input id="caption" name="caption" class="block mt-1 w-full" type="text" required />
-                             <x-input-error :messages="$errors->get('caption')" class="mt-2" />
-                         </div>
-                         <x-primary-button class="mt-4">Upload</x-primary-button>
-                     </form>
-                 </div>
-             </div>
-         </div>
-     </div>
-   @else
-     <p class="text-center">Anda belum login</p>
-   @endif
- </div>
+  <div class="modal-overlay absolute w-full h-full bg-gray-900 opacity-50"></div>
+  <div class="modal-container bg-white w-11/12 md:max-w-md mx-auto rounded shadow-lg z-50 overflow-y-auto transform transition-all duration-300 scale-0">
+    @if(@isset(Auth::user()->id))
+      <div class="upload-form">
+          <div class="max-w-xl mx-auto sm:px-2 glass">
+              <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                  <div class="p-6 md:p-6 text-gray-900 ">
+                      <form action="{{ route('post.store') }}" method="post" enctype="multipart/form-data">
+                          @csrf
+                          <input 
+                              type="text" 
+                              name="user_id" 
+                              value="{{ Auth::user()->id }}" 
+                              class="hidden"
+                          >
+                          <input type="file" name="image">
+                          <div class="mt-4">
+                              <x-input-label for="Caption" :value="__('Caption')" />
+                              <x-text-input id="caption" name="caption" class="block mt-1 w-full" type="text" required />
+                              <x-input-error :messages="$errors->get('caption')" class="mt-2" />
+                          </div>
+                          <div class="flex justify-between mt-4">
+                            <x-primary-button class="bg-green-500 hover:bg-green-700">Upload</x-primary-button>
+                            <button id="cancelModal" type="button" class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">Cancel</button>
+                          </div>
+                      </form>
+                  </div>
+              </div>
+          </div>
+      </div>
+    @else
+      <p class="text-center">Anda belum login</p>
+    @endif
+  </div>
 </div>
 
 <script>
  const openModal = document.getElementById('openModal');
  const modal = document.getElementById('myModal');
- const closeModal = document.getElementById('closeModal');
+ const closeModal = document.getElementById('cancelModal');
  const modalOverlay = document.querySelector('.modal-overlay');
  const modalContainer = document.querySelector('.modal-container');
 
@@ -49,8 +52,7 @@
  });
 
  closeModal.addEventListener('click', function () {
-   modalContainer.classList.remove('scale-100');
-   modal.classList.add('hidden');
+   closeModalFunction();
  });
 
  modalOverlay.addEventListener('click', function (e) {
